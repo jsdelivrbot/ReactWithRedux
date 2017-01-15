1b15de24958b7966696b14fd59be1ff61cb3c1da
alexkmoto <alexkmoto@gmail.com>
2017-01-15 14:53:38 -0600

    updated search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 ////When user inputs, search Youtube
 //const SearchBar = () => {
 //  return <input />
 //};
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={this.onInputChange} />;
+  }
+
+  //event handler for input. The event variable is returned by the event handler
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
