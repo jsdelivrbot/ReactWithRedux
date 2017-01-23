3f4afe6f619621b7bca2143a100530dfe5bc5a59
alexkmoto <alexkmoto@gmail.com>
2017-01-22 22:25:28 -0600

    Requests from Youtube

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -10,12 +11,10 @@ const API_KEY = 'AIzaSyB7Y3CfpSS1bsKDKMUiAOteQWIJEvOcdhY';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       videos: [],
       selectedVideo: null
     };
-
     this.videoSearch('surfboards');
   }
 
@@ -29,9 +28,10 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
